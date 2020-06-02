66f1c23124cb175ded3da2e5e27d21d4fd34edc5
Superman696 <61060058+Superman696@users.noreply.github.com>
2020-06-02 22:02:58 +0300

    added spring config

---
+++ src/main/java/intro/config/AppConfig.java
@@ -0,0 +1,47 @@
+package intro.config;
+
+import intro.dao.MyService;
+import intro.model.User;
+import java.util.Properties;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+@Configuration
+@ComponentScan(basePackages = {
+        "intro.service",
+        "intro.dao"
+})
+public class AppConfig {
+    @Bean
+    public MyService getMyService() {
+        return () -> System.out.println("Hello, Human");
+    }
+
+    @Bean
+    public DataSource getDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost/intro?serverTimezone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("MateAcademy2020");
+        return dataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean getSessionFactory() {
+        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+        factoryBean.setDataSource(getDataSource());
+
+        Properties properties = new Properties();
+        properties.put("hibernate.show_sql", "true");
+        properties.put("hibernate.hbm2ddl.auto", "create-drop");
+        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+        factoryBean.setHibernateProperties(properties);
+        factoryBean.setAnnotatedClasses(User.class);
+        return factoryBean;
+    }
+}

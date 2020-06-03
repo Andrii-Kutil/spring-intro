355eef9d4ea01b8a7dd2033879090ea64db1d1fc
Andrii <61060058+Andrii-Kutil@users.noreply.github.com>
2020-06-03 11:54:04 +0300

    Hw 30 (#1)
    * added user service and dao
    
    * tested Bean annotation
    
    * added spring config
    
    * added dependencies
    
    * Update pom.xml
    
    added space
    
    * added space in pom.xml
    
    * deleted exception package
    
    * deleted some dependencies, used try-with-resources and added personal exception
    
    * deleted some dependencies, used try-with-resources and added personal exception
    
    * fixed mistakes
    
    Co-authored-by: Superman696 <61060058+Superman696@users.noreply.github.com>

---
+++ src/main/java/intro/Main.java
@@ -0,0 +1,27 @@
+package intro;
+
+import intro.config.AppConfig;
+import intro.model.User;
+import intro.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        User user1 = new User("Andrii", 24);
+        User user2 = new User("Sasha", 23);
+        User user3 = new User("Dima", 22);
+        User user4 = new User("Masha", 1);
+        User user5 = new User("Olga", 4);
+        User user6 = new User("Marina", 67);
+        UserService userService = context.getBean(UserService.class);
+        userService.add(user1);
+        userService.add(user2);
+        userService.add(user3);
+        userService.add(user4);
+        userService.add(user5);
+        userService.add(user6);
+        userService.listUsers().forEach(System.out::println);
+    }
+}
---
+++ src/main/java/intro/dao/impl/UserDaoImpl.java
@@ -0,0 +1,38 @@
+package intro.dao.impl;
+
+import intro.dao.UserDao;
+import intro.exception.DataProcessingException;
+import intro.model.User;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public void add(User user) {
+        try (Session session = sessionFactory.openSession()) {
+            session.save(user);
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't save user", e);
+        }
+    }
+
+    @Override
+    public List<User> listUsers() {
+        String hql = "FROM User";
+        try (Session session = sessionFactory.openSession()) {
+            Query<User> query = session.createQuery("FROM User", User.class);
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get list of users", e);
+        }
+    }
+}
